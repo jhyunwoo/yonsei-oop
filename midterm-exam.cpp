0e2199630932b5c8c52c41e96bbd6d6a1194d20e
jhyunwoo <jhyunwoo0228@gmail.com>
2024-10-20 23:19:29 +0900

    recap

--- midterm-exam.cpp
+++ midterm-exam.cpp
@@ -5,6 +5,60 @@
 using namespace std; // std namespace를 사용
 
 void printTitle(const string& title);
+void square(int& n);
+void justPrintIt(int& n);
+float average(int n1=1, int n2=1); // Default value -> average(4)로 call 하면 4와 1의 평균 return
+float average(int n1, int n2, int n3);
+float arrayAverage(int n[], int size);
+
+class User
+{
+public:
+    void output();
+    User(string name, int age, int point, float height, string school);
+    User();
+    static void countryOut();
+    static string country;
+private:
+    string name;
+    int age;
+    int point;
+    float height;
+    string school;
+};
+
+class Item
+{
+public:
+    Item(string name, int level);
+    int getLevel() const;
+    string getName() const;
+    friend bool operator ==(const Item& item1, const Item& item2)
+    {
+        return item1.level == item2.level && item1.name == item2.name;
+    }
+    friend ostream& operator <<(ostream& outputStream, const Item& item)
+    {
+        return outputStream << item.name << " " << item.level;
+    }
+    friend istream& operator >>(istream& inputStream, Item& item)
+    {
+        return inputStream >> item.name >> item.level;
+    }
+
+private:
+    string name;
+    int level;
+};
+
+const Item operator +(const Item& item1, const Item& item2)
+{
+    int totalLevel = item1.getLevel() + item2.getLevel();
+    return Item("New", totalLevel);
+}
+
+
+string User::country = "Korea";
 
 int main()
 {
@@ -159,6 +213,92 @@ int main()
         cout << "This sentence is not prompted!" << endl;
     }
 
+    /* Random Number Generator*/
+    printTitle("Random Number Generator");
+    cout << rand() << endl;
+    cout << rand()%6 << endl;
+    cout << rand()%6+1 << endl;
+
+    /* Random Number Generator with Seed*/
+    srand(time(0));
+    cout << rand() << endl;
+    cout << rand() << endl;
+
+    cout << "Random Number between 0.0 & 1.0" << endl;
+    cout << (RAND_MAX - rand())/static_cast<double>(RAND_MAX) << endl;
+
+    /* Call by Reference */
+    printTitle("Call by Reference");
+    int data = 2;
+    square(data);
+    cout << data << endl;
+
+    /* Overloading */
+    printTitle("Overloading");
+    cout << average(1,2) << endl;
+    cout << average(2,3,5) << endl;
+
+    /* Array */
+    printTitle("Array");
+    int scores[4] = {4,2,10,4};
+    int b[] = {1,2,3,4,5,6,7}; // b array 사이즈는 자동으로 7이 됨
+
+    cout <<"scores 배열 주소: "<< scores << endl;
+    int totalScore = 0;
+    for(int i=0; i < 4; i++)
+    {
+        cout << scores[i] << endl;
+        totalScore += scores[i];
+    }
+    cout << "Total Score: " << totalScore << endl;
+
+    /* Structure */
+    printTitle("Structure");
+    struct Person
+    {
+        string name;
+        int age;
+        int point;
+        float height;
+    };
+
+    Person user = {"Hyunwoo", 20, 10, 180.7};
+    cout << user.age << endl;
+
+    /* Class */
+    printTitle("Class");
+    User user1 = User("Hyunwoo", 20, 0, 180.7, "Yonsei Univ.");
+    user1.output();
+    User anonymous; // argument 없이 class 선언
+    anonymous.output();
+
+    /* Static Member and Function */
+    printTitle("Static Member and Function");
+    user1.countryOut();
+    anonymous.countryOut();
+    User::countryOut();
+
+    User::country = "UK";
+    user1.countryOut();
+    anonymous.countryOut();
+    User::countryOut();
+
+    /* Operator in class */
+    Item item1 = Item("Hello", 1);
+    Item item2 = Item("world", 2);
+    Item upgrade = item1 + item2;
+    cout << upgrade.getLevel() << endl;
+    Item newItem = Item("New", 3);
+    cout << newItem.getLevel() << endl;
+    cout << upgrade.getLevel() << endl;
+    cout << newItem.getName() << endl;
+    cout << upgrade.getName() << endl;
+    cout << (upgrade == newItem) << endl;
+
+    cout << "Update Item: ";
+    cin >> newItem;
+    cout << newItem << endl;
+
     return 0; // 함수의 종료를 명시적으로 표현
 }
 
@@ -166,3 +306,65 @@ void printTitle(const string& title)
 {
     cout << "---------- " << title << " ----------" << endl;
 }
+
+void square(int& n)
+{
+    n = n*n; // 주소 값을 입력 받아 해당 변수의 제곱을 변수에 재할당
+}
+
+void justPrintIt(const int& n)
+{
+    cout << n << endl; // n에 있는 값을 수정하지 않고 읽기만 함 (const를 통해 명시)
+}
+
+// 입력 2개인 경우 실행
+float average(int n1, int n2)
+{
+    return (float)(n1+n2)/2;
+}
+// Overloading 입력 3개인 경우 실행
+float average(int n1, int n2, int n3)
+{
+    return (float)(n1+n2+n3)/3;
+}
+
+float arrayAverage(int n[], int size)
+{
+    int total = 0;
+    for(int i=0; i < size; i++)
+    {
+        total += n[i];
+    }
+    return static_cast<float>(total)/size;
+}
+
+void User::output()
+{
+    cout << "Name: " << name << endl;
+}
+
+User::User(string name, int age, int point, float height, string school):name(name), age(age), point(point), height(height), school(school)
+{}
+
+User::User():name("none"), age(0), point(0), height(0), school("none"){}
+
+void User::countryOut()
+{
+    cout << country << endl;
+}
+
+Item::Item(string name, int level):name(name), level(level)
+{
+}
+
+int Item::getLevel() const
+{
+    return level;
+}
+
+string Item::getName() const
+{
+    return name;
+}
+
+
